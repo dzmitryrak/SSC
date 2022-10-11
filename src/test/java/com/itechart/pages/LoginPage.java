1423768d84a1a64e50da5b624b406f6fe476a9f8
dzmitry.viachaslavau <6870064@gmail.com>
2022-10-11 16:23:50 +0300

    ITA-19 removal of Steps, editing of AciertoTest

--- src/test/java/com/itechart/pages/LoginPage.java
+++ src/test/java/com/itechart/pages/LoginPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Step;
 import lombok.extern.log4j.Log4j2;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
