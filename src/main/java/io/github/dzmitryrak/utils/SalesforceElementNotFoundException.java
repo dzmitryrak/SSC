7cc8707bc66697a42a5d573696b8fb9298c5d865
Vadzim Trafimenkau <vtrafimenkau@mwiah.co.uk>
2022-12-14 21:41:22 +0300

    added logging and screenshotting to SalesforceElementNotFoundException class

--- src/main/java/io/github/dzmitryrak/utils/SalesforceElementNotFoundException.java
+++ src/main/java/io/github/dzmitryrak/utils/SalesforceElementNotFoundException.java
@@ -1,11 +1,32 @@
 package io.github.dzmitryrak.utils;
 
+import lombok.extern.log4j.Log4j2;
+
+import static com.codeborne.selenide.Selenide.screenshot;
+
+@Log4j2
 public class SalesforceElementNotFoundException extends RuntimeException {
     public SalesforceElementNotFoundException(String errorMessage) {
         super(errorMessage);
+        log.warn("Unable to find element.");
+        log.debug("Error message: {}", errorMessage);
+        screenshot("SF Exception State " + errorMessage);
     }
 
     public SalesforceElementNotFoundException(String errorMessage, Throwable err) {
         super(errorMessage, err);
+        log.warn("Unable to find element.");
+        log.debug("Custom error message: {}", errorMessage);
+        log.debug("System error message: {}", err.getMessage());
+        log.debug(err.getStackTrace());
+        screenshot("SF Exception State " + errorMessage);
+    }
+
+    public SalesforceElementNotFoundException(Throwable err) {
+        super(err);
+        log.warn("Unable to find element.");
+        log.debug("System error message: {}", err.getMessage());
+        log.debug(err.getStackTrace());
+        screenshot("SF Exception State " + err.getMessage());
     }
 }
