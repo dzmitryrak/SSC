872c2397aea36c1970daa664fa50e64a670f9f70
Olga <olka030389@yandex.ru>
2025-05-26 11:08:06 +0300

    updated create contact test

--- src/test/java/io/github/dzmitryrak/tests/ContactCRUDTest.java
+++ src/test/java/io/github/dzmitryrak/tests/ContactCRUDTest.java
@@ -28,7 +28,7 @@ public class ContactCRUDTest extends BaseTest {
             put("Fax", "test");
             put("Birthdate", "20.05.1990");
             put("Email", faker.internet().emailAddress());
-            put("Reports To", "Lola Joka");
+            put("Reports To", "Aldo Glover Koepp");
             put("Assistant", "test");
             put("Lead Source", "Other");
             put("Asst. Phone", faker.phoneNumber().phoneNumber());
