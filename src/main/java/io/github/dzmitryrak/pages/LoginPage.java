dae7bccac2a9e4c84a35e4e3855977483c4ab935
Olga <olka030389@yandex.ru>
2025-05-27 11:14:05 +0300

    updated login page

--- src/main/java/io/github/dzmitryrak/pages/LoginPage.java
+++ src/main/java/io/github/dzmitryrak/pages/LoginPage.java
@@ -50,6 +50,7 @@ public class LoginPage extends BasePage {
         $(USERNAME_LOCATOR).setValue(username);
         $(PASSWORD_LOCATOR).setValue(password);
         $(LOGIN_BUTTON_LOCATOR).click();
+        waitForPageLoaded();
     }
 
     /**
